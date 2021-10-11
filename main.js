4a943d8d0bb3e8c70423e2a89094096ae0fdf335
Anya-Mo <72193720+Anya-Mo@users.noreply.github.com>
2021-10-11 20:25:42 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+//Teachable Link (https://teachablemachine.withgoogle.com/models/XmYyR4aeK/)
+function preload() {
+
+}
+function setup() {
+    canvas=createCanvas(300,250);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+    classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/XmYyR4aeK/model.json",modelLoaded());
+}
+function draw() {
+    image(video,0,0,300,250);
+    classifier.classify(video,gotResults);
+}
+function modelLoaded() {
+    console.log("Model Loaded");
+}
+function gotResults(error,results) {
+    if (error) {
+        console.error(error);
+    }
+    else {
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML=results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML=results[0].confidence;
+    }
+}
